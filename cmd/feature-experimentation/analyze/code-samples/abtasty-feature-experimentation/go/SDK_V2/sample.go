14c8ebc2fa51cd3fcefb97d71f129a09b3bf421a
Chadi Laoulaou <49269946+Chadiii@users.noreply.github.com>
2024-10-22 14:20:11 +0200

    Feat/update codebase analyzer v0.9.2 (#4)
    * feat: update codebase-analyzer library
    
    * chore(GOMOD): Update go mod with new codebase analyzer version
    
    * chore(SDK example): Add SDK V4 examples

--- cmd/feature-experimentation/analyze/code-samples/abtasty-feature-experimentation/go/SDK_V2/sample.go
+++ cmd/feature-experimentation/analyze/code-samples/abtasty-feature-experimentation/go/SDK_V2/sample.go
@@ -2,10 +2,10 @@ package SDK_V2
 
 func main() {
 	// Using the Decision API (default)
-	fsClient, _ := flagship.Start("environmentID", "apiKey")
+	fsClient, err := flagship.Start(environmentID, apiKey)
 
 	// Using the Bucketing mode
-	fsClient, _ = flagship.Start("environmentID", "apiKey", client.WithBucketing())
+	fsClient, err := flagship.Start(environmentID, apiKey, client.WithBucketing())
 
 	// Create visitor context
 	context := map[string]interface{}{
@@ -14,11 +14,11 @@ func main() {
 		"name":  "visitor",
 	}
 	// Create a visitor
-	fsVisitor, _ := fsClient.NewVisitor("visitor_id", context)
+	fsVisitor, err := fsClient.NewVisitor("visitor_id", context)
 
 	// Update a single key
 	fsVisitor.UpdateContextKey("vipUser", true)
-	fsVisitor.UpdateContextKey("age", 30)
+	fsVisitor.UpdateContextLey("age", 30)
 
 	// Update the whole context
 	newContext := map[string]interface{}{
@@ -32,9 +32,6 @@ func main() {
 	discountName, err := fsVisitor.GetModificationNumber("btnSize", 13, true)
 	discountName, err := fsVisitor.GetModificationBool("showBtn", false, true)
 
-	// these flags will be analyzed using the custom-regex file example-regex.json
-	// try the command: flagship analyze flag list --custom-regex-json ./cmd/analyze/flag/example-regex.json --directory ./cmd/analyze/code-samples/abtasty-cli/go/
-	flagValue, _ := example.BoolVariation("my-boolean-flag", false)
-	flagValue1, _ := example.StringVariation("my-string-flag", "defaltVal")
-	flagValue2, _ := example.Float64Variation("my-numbers-flag", 13.6)
+	// If there is not error (and if there is, your value will still be set to defaut), you can use your modification value in your business logic
+	discountValue := getDiscountFromDB(discountName)
 }
