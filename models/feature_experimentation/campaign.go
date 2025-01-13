b4c9ddfd62d88325794e01364f2855d8824aca71
Chadi LAOULAOU <chadilaoulaou@gmail.com>
2025-01-13 13:05:19 +0100

    fix: unified models for requesting & resource loader, add pointer for struct to avoid having empty struct when marshaling

--- models/feature_experimentation/campaign.go
+++ models/feature_experimentation/campaign.go
@@ -1,14 +1,20 @@
 package feature_experimentation
 
 type CampaignFE struct {
-	Id              string           `json:"id,omitempty"`
-	ProjectId       string           `json:"project_id"`
-	Name            string           `json:"name"`
-	Description     string           `json:"description"`
-	Type            string           `json:"type"`
-	Status          string           `json:"status"`
-	VariationGroups []VariationGroup `json:"variation_groups"`
-	Scheduler       Scheduler        `json:"scheduler"`
+	Id              string            `json:"id,omitempty"`
+	ProjectId       string            `json:"project_id"`
+	Name            string            `json:"name"`
+	Description     string            `json:"description"`
+	Type            string            `json:"type,omitempty"`
+	Status          string            `json:"status,omitempty"`
+	State           string            `json:"state,omitempty"`
+	Slug            string            `json:"slug,omitempty"`
+	VariationGroups []*VariationGroup `json:"variation_groups"`
+	Scheduler       *Scheduler        `json:"scheduler,omitempty"`
+	PrimaryGoal     *Goal             `json:"primary_goal,omitempty"`
+	CreatedAt       string            `json:"created_at,omitempty"`
+	UpdatedAt       string            `json:"updated_at,omitempty"`
+	SecondaryGoals  []*Goal           `json:"secondary_goals,omitempty"`
 }
 
 type Scheduler struct {
--- models/feature_experimentation/variation_group.go
+++ models/feature_experimentation/variation_group.go
@@ -1,22 +1,36 @@
 package feature_experimentation
 
 type VariationGroup struct {
-	Id         string        `json:"id,omitempty"`
-	Name       string        `json:"name,omitempty"`
-	Variations []VariationFE `json:"variations"`
-	Targeting  Targeting     `json:"targeting"`
+	Id               string            `json:"id,omitempty"`
+	Name             string            `json:"name"`
+	Variations       []*VariationFE    `json:"variations"`
+	Targeting        *Targeting        `json:"targeting"`
+	AllocationConfig *AllocationConfig `json:"allocation_config,omitempty"`
 }
 
 type Targeting struct {
-	TargetingGroups []TargetingGroup `json:"targeting_groups"`
+	TargetingGroups []*TargetingGroup `json:"targeting_groups"`
 }
 
 type TargetingGroup struct {
-	Targetings []InnerTargeting `json:"targetings"`
+	Targetings []*InnerTargeting `json:"targetings"`
 }
 
 type InnerTargeting struct {
 	Key      string      `json:"key"`
 	Operator string      `json:"operator"`
 	Value    interface{} `json:"value"`
+}
+
+type AllocationConfig struct {
+	StartDate       string         `json:"start_date"`
+	Timezone        string         `json:"timezone"`
+	StartAllocation float64        `json:"start_allocation"`
+	PeriodicSteps   *PeriodicSteps `json:"periodic_steps"`
+}
+
+type PeriodicSteps struct {
+	Allocation float64 `json:"allocation"`
+	Step       int     `json:"step"`
+	Step_type  string  `json:"step_type"`
 }
