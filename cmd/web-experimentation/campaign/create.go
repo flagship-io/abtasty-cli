9af8deb6accd47044fde54735cb3bf00e571903d
Chadi LAOULAOU <chadilaoulaou@gmail.com>
2025-05-20 11:59:20 +0200

    feat(campaign): add creating campaign function

--- cmd/web-experimentation/campaign/create.go
+++ cmd/web-experimentation/campaign/create.go
@@ -7,8 +7,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"strconv"
 	"strings"
 
+	"github.com/flagship-io/abtasty-cli/models/web_experimentation"
 	model "github.com/flagship-io/abtasty-cli/models/web_experimentation"
 	httprequest "github.com/flagship-io/abtasty-cli/utils/http_request"
 	we "github.com/flagship-io/abtasty-cli/utils/http_request/web_experimentation"
@@ -16,44 +18,44 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// createCmd represents the create command
-var createCmd = &cobra.Command{
-	Use:   "create [-d <data-raw> | --data-raw=<data-raw>]",
-	Short: "Create a campaign",
-	Long:  `Create a campaign`,
-	Run: func(cmd *cobra.Command, args []string) {
-		var campaignModel model.CampaignWEResourceLoader
+func CreateCampaign(rawData []byte) []byte {
+	var campaignModel model.CampaignWEResourceLoader
 
-		err := json.Unmarshal([]byte(DataRaw), &campaignModel)
-		if err != nil {
-			log.Fatalf("error occurred: %s", err)
-		}
+	err := json.Unmarshal(rawData, &campaignModel)
+	if err != nil {
+		log.Fatalf("error occurred: %s", err)
+	}
 
-		campaignCommon, _ := json.Marshal(struct {
-			Name        string `json:"name"`
-			Url         string `json:"url"`
-			Description string `json:"description"`
-			Type        string `json:"type"`
-		}{
-			Name:        campaignModel.Name,
-			Url:         campaignModel.Url,
-			Description: campaignModel.Description,
-			Type:        campaignModel.Type,
-		})
+	campaignCommon, _ := json.Marshal(struct {
+		Name        string `json:"name"`
+		Url         string `json:"url"`
+		Description string `json:"description"`
+		Type        string `json:"type"`
+	}{
+		Name:        campaignModel.Name,
+		Url:         campaignModel.Url,
+		Description: campaignModel.Description,
+		Type:        campaignModel.Type,
+	})
 
-		campaignPatch, _ := json.Marshal(struct {
-			Traffic            int    `json:"traffic,omitempty"`
-			GlobalCodeCampaign string `json:"global_code,omitempty"`
-		}{
-			Traffic:            campaignModel.Traffic,
-			GlobalCodeCampaign: campaignModel.GlobalCodeCampaign,
-		})
+	campaignPatch, _ := json.Marshal(struct {
+		Traffic            int    `json:"traffic,omitempty"`
+		GlobalCodeCampaign string `json:"global_code,omitempty"`
+	}{
+		Traffic:            campaignModel.Traffic,
+		GlobalCodeCampaign: campaignModel.GlobalCodeCampaign,
+	})
 
-		campaignHeader, err := httprequest.CampaignWERequester.HTTPCreateCampaign(campaignCommon)
-		parts := strings.Split(string(campaignHeader), "/")
-		campaignID := parts[len(parts)-1]
-		fmt.Println(campaignID)
+	campaignHeader, err := httprequest.CampaignWERequester.HTTPCreateCampaign(campaignCommon)
+	parts := strings.Split(string(campaignHeader), "/")
+	campaignID := parts[len(parts)-1]
 
+	campaignIDInt, err := strconv.Atoi(campaignID)
+	if err != nil {
+		log.Fatalf("error occurred: %v", err)
+	}
+
+	if campaignModel.CampaignTargeting != nil {
 		model := we.JsonModelToModel(*campaignModel.CampaignTargeting)
 
 		parsedModel, err := json.Marshal(model)
@@ -65,15 +67,16 @@ var createCmd = &cobra.Command{
 		if err != nil {
 			log.Fatalf("error occurred: %v", err)
 		}
+	}
 
-		campaignEdit, err := httprequest.CampaignWERequester.HTTPEditCampaign(campaignID, campaignPatch)
-		if err != nil {
-			log.Fatalf("error occurred: %v", err)
-		}
+	_, err = httprequest.CampaignWERequester.HTTPEditCampaign(campaignID, campaignPatch)
+	if err != nil {
+		log.Fatalf("error occurred: %v", err)
+	}
 
-		fmt.Fprintf(cmd.OutOrStdout(), "%s\n", string(campaignEdit))
-
+	if len(campaignModel.Variations) != 0 {
 		for _, v := range campaignModel.Variations {
+			varGlobalCode := v.GlobalCodeVariation
 			variation := struct {
 				Name        string `json:"name,omitempty"`
 				Description string `json:"description,omitempty"`
@@ -96,7 +99,6 @@ var createCmd = &cobra.Command{
 
 			parts := strings.Split(string(variationHeader), "/")
 			variationID := parts[len(parts)-1]
-			fmt.Println(variationID)
 
 			variationPatch := struct {
 				Traffic int `json:"traffic,omitempty"`
@@ -113,12 +115,78 @@ var createCmd = &cobra.Command{
 			if err != nil {
 				log.Fatalf("error occurred: %v", err)
 			}
-		}
 
-		//err = httprequest.CampaignWERequester.HTTPDeleteCampaign(campaignID)
+			variationIDInt, err := strconv.Atoi(variationID)
+			if err != nil {
+				log.Fatalf("error occurred: %v", err)
+			}
+
+			if varGlobalCode.Js != "" {
+				modificationToPush := web_experimentation.ModificationCodeCreateStruct{
+					InputType:   "modification",
+					Name:        "",
+					Value:       string(varGlobalCode.Js),
+					Selector:    "",
+					Type:        "customScriptNew",
+					Engine:      string(varGlobalCode.Js),
+					VariationID: variationIDInt,
+				}
+
+				_, err = httprequest.ModificationRequester.HTTPCreateModification(campaignIDInt, modificationToPush)
+				if err != nil {
+					log.Fatalf("error occurred: %v", err)
+				}
+			}
+
+			if varGlobalCode.Css != "" {
+				modificationToPush := web_experimentation.ModificationCodeCreateStruct{
+					InputType:   "modification",
+					Name:        "",
+					Value:       string(varGlobalCode.Css),
+					Selector:    "",
+					Type:        "addCSS",
+					Engine:      string(varGlobalCode.Css),
+					VariationID: variationIDInt,
+				}
+
+				_, err := httprequest.ModificationRequester.HTTPCreateModification(campaignIDInt, modificationToPush)
+				if err != nil {
+					log.Fatalf("error occurred: %v", err)
+				}
+			}
+		}
+	}
+
+	//delete origin variation
+	body, err := httprequest.CampaignWERequester.HTTPGetCampaign(campaignID)
+	if err != nil {
+		log.Fatalf("error occurred: %s", err)
+	}
+
+	if body.Variations[0].Id != 0 {
+		err = httprequest.VariationWERequester.HTTPDeleteVariation(campaignIDInt, body.Variations[0].Id)
 		if err != nil {
 			log.Fatalf("error occurred: %v", err)
 		}
+	}
+
+	bodyByte, err := json.Marshal(body)
+	if err != nil {
+		log.Fatalf("error occurred: %s", err)
+	}
+
+	return bodyByte
+}
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create [-d <data-raw> | --data-raw=<data-raw>]",
+	Short: "Create a campaign",
+	Long:  `Create a campaign`,
+	Run: func(cmd *cobra.Command, args []string) {
+		resp := CreateCampaign([]byte(DataRaw))
+
+		fmt.Fprintln(cmd.OutOrStdout(), string(resp))
 	},
 }
 
